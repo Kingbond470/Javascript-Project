aa3cf3d00e5d72eb47462b101621ef0002790ca5
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2020-10-14 23:16:36 +0530

    Qustion and Answer Page
    Q&N about anime
    use fontawesome5.2 for getting answer and effect

---
+++ Question and Answer Page- Anime/app.js
@@ -0,0 +1,16 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
